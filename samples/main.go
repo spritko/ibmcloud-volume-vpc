002cbd61d2f5a225710271817183754ca3c072a2
Sameer-Shaikh <sameer.shaikh@ibm.com>
2021-03-19 18:33:44 +0530

    Review Comments

--- samples/main.go
+++ samples/main.go
@@ -111,7 +111,7 @@ func main() {
 	} else if conf.Softlayer.SoftlayerFileEnabled {
 		providerName = conf.Softlayer.SoftlayerFileProviderName
 	} else if conf.VPC.Enabled {
-		providerName = conf.VPC.VPCProviderType
+		providerName = conf.VPC.VPCVolumeType
 	}
 
 	valid := true
