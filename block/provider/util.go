49bd2ea2e50e7d15ceef40384c4b07c74789c5b1
Sameer Shaikh <sameer.shaikh@ibm.com>
2021-09-02 16:36:56 +0530

    Performance fix

--- block/provider/util.go
+++ block/provider/util.go
@@ -35,7 +35,7 @@ var maxRetryAttempt = 10
 var maxRetryGap = 60
 
 // retryGap ...
-var retryGap = 10
+//var retryGap = 10
 
 //ConstantRetryGap ...
 const (
@@ -70,7 +70,7 @@ var skipErrorCodes = map[string]bool{
 // retry ...
 func retry(logger *zap.Logger, retryfunc func() error) error {
 	var err error
-	retryGap = 10
+	retryGap := 10
 	for i := 0; i < maxRetryAttempt; i++ {
 		if i > 0 {
 			time.Sleep(time.Duration(retryGap) * time.Second)
@@ -166,7 +166,7 @@ func NewFlexyRetry(maxRtyAtmpt int, maxrRtyGap int) FlexyRetry {
 func (fRetry *FlexyRetry) FlexyRetry(logger *zap.Logger, funcToRetry func() (error, bool)) error {
 	var err error
 	var stopRetry bool
-	retryGap = 10
+	retryGap := 10
 	for i := 0; i < fRetry.maxRetryAttempt; i++ {
 		if i > 0 {
 			time.Sleep(time.Duration(retryGap) * time.Second)
